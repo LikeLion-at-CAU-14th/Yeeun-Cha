649f2f8da98cfd93ab13b8bfafc0575f65bde84c
yeankong <chayaeeun25@gmail.com>
2026-03-30 06:37:41 +0900

    feat: 모델추가 및 연습

--- likelion14/posts/models.py
+++ likelion14/posts/models.py
@@ -10,6 +10,13 @@ class BaseModel(models.Model): # models.Model을 상속받음
         abstract = True
 
 
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Post(BaseModel): # BaseModel을 상속받음
 
     CHOICES = (
@@ -22,6 +29,17 @@ class Post(BaseModel): # BaseModel을 상속받음
     content = models.TextField()
     status = models.CharField(max_length=10, choices=CHOICES, default='STORED')
     writer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
+    categories = models.ManyToManyField(Category, related_name='posts')
 
     def __str__(self):
         return self.title
+
+
+
+class Comment(BaseModel):
+    id = models.AutoField(primary_key=True)
+    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return self.content[:20]
